3119946c148568ea95794c241ba31adaea26e003
Taylor Peat <taylorpeat@hotmail.com>
2015-10-28 01:31:03 -0400

    Revisions based on code review

--- oo_ttt.rb
+++ oo_ttt.rb
@@ -1,139 +1,164 @@
+module GameEvaluation
+  WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+  def moves_for(current_board, symbol)
+    current_board.each_index.select { |idx| current_board[idx] == symbol }
+  end
+end
+
 class Game
+  attr_accessor :winner
+  attr_reader :human, :computer, :game_board
+   
   def initialize
     @human = Human.new
     @computer = Computer.new
     @game_board = Board.new
+    @winner = nil
   end
 
   def play
     loop do
-      @game_board.update_current_board(@human.select_square(@game_board.square_index), :x)
-      @game_board.update_display
-      break if @game_board.check_winner?(:x)
-      @game_board.update_current_board(@computer.select_square(@game_board.current_board), :o)
-      @game_board.update_display
-      break if @game_board.check_winner?(:o)
+      loop do
+        game_board.update_current_board(get_human_choice)
+        break if self.winner = game_board.check_winner(:x)
+        game_board.update_current_board(get_computer_choice)
+        break if self.winner = game_board.check_winner(:o)
+      end
+
+      display_winner_message
+      break if replay?
+      initialize
     end
-    display_winner_message
-    replay
+  end
+
+  def get_human_choice
+    square = human.select_square(game_board.current_board) - 1
+    symbol = :x
+    [square, symbol]
+  end
+
+  def get_computer_choice
+    square = computer.select_square(game_board.current_board)
+    symbol = :o
+    [square, symbol]
+  end
+
+  def replay?
+    puts "Enter 'y' if you would like to play again"
+    gets.chomp.downcase != 'y'
   end
 
   def display_winner_message
     puts
     sleep 1
-    if @game_board.winner == :x
+    if winner == :x
       puts "Congratulations! You won!"
       sleep 1
       puts "\nOn easy...\n\n"
       sleep 1
-    elsif @game_board.winner == :o
+    elsif winner == :o
       puts "You lost at tic-tac-toe... that's embarrasing.\n\n"
       sleep 1
     else
       puts "Tied. Try it on easy if you feel like winning...\n\n"
       sleep 1
-      if @computer.difficulty == "easy"
+      if computer.difficulty == "easy"
         sleep 1
-        puts "Oh wait... you are on easy... ouch\n\n"
+        puts "Oh wait... you were playing on easy... ouch\n\n"
         sleep 1
       end
     end
-  end
-
-  def replay
-    continue = nil
-    loop do
-      puts "Would you like to play again? (Y/N)"
-      continue = gets.chomp.downcase
-      ['y', 'n'].include?(continue) ? break : next
-    end
-    Game.new.play if continue == 'y'
   end
 end
 
 
 class Board
-  WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  include GameEvaluation
 
   DISPLAY_TEMPLATE = { x: ['   .   .   ', '    \ /    ', '     /     ', '    / \    ', '   .   .   '],
                      o: ['   .--.    ', '  :    :   ', '  |    |   ', '  :    ;   ', '   `--\'    '],
                      b: [' ' * 11, ' ' * 11, ' ' * 11, ' ' * 11, ' ' * 11] }
   
-  attr_reader :current_board, :square_index, :winner
+  attr_reader :current_board
 
   def initialize
     @current_board = initialize_board
-    @square_index = %w(1 2 3 4 5 6 7 8 9)
-    @winner = nil
     update_display
   end
 
   def initialize_board
-    starting_board = {}
-    (1..9).each {|position| starting_board[position] = :b}
+    starting_board = []
+    (0..8).each {|position| starting_board[position] = :b}
     starting_board
+  end
+
+  def update_current_board(selected_square_info)
+    current_board[selected_square_info[0]] = selected_square_info[1]
+    update_display
   end
 
   def update_display
     system 'clear' or system 'cls'
-    i = 1
+    print_main_board
+    puts "Select an available location between 1 and 9\n\n"
+    print_ui_board
+  end
+
+  def print_main_board
     puts
-    loop do
-      j = 0
-      5.times do
-        puts "#{DISPLAY_TEMPLATE[@current_board[i]][j]}|#{DISPLAY_TEMPLATE[@current_board[i+1]][j]}"\
-             "|#{DISPLAY_TEMPLATE[@current_board[i+2]][j]}"
-        j += 1
+    printing_array = [current_board.values_at(0..2),
+                      current_board.values_at(3..5),
+                      current_board.values_at(6..8)]
+    printing_array.each_with_index do |row_array, row_index|
+      print_row(row_array)
+      puts "-----------+-----------+-----------" unless row_index == 2
+    end
+    puts
+  end
+
+  def print_row(row_array)
+    5.times do |i|
+      row_array.each_with_index do |square_value, square_index|
+        print DISPLAY_TEMPLATE[square_value][i]
+        print square_index == 2 ? "\n" : "|"
       end
-      break if i > 4
-      puts "-----------+-----------+-----------"
-      i += 3
     end
-    spacer = ' ' * 15
-    puts
-    puts "Select an available location between 1 and 9"
-    puts format("\n%s %s | %s | %s\n%s---+---+---\n%s %s | %s | %s\n%s---+---+---\n%s %s | %s | %s\n",
-                spacer, @square_index[0], @square_index[1], @square_index[2], spacer, spacer, @square_index[3], @square_index[4], @square_index[5], spacer, spacer,
-                @square_index[6], @square_index[7], @square_index[8])
   end
 
-  def check_winner?(x_or_o)
-    player_numbers = @current_board.select { |num, sq| sq == x_or_o }.keys
+  def print_ui_board
+    current_board.each_with_index do |square_value, square_index|
+      print square_value == :b ? " #{(square_index + 1)} " : "   "
+      print [2, 5, 8].include?(square_index) ? "\n" : "|"
+      print "---+---+---\n" if [2, 5].include?(square_index)
+    end
+  end
+
+  def check_winner(x_or_o)
+    player_squares = moves_for(current_board, x_or_o)
     WINNING_COMBOS.any? do |combo|
-      @winner = x_or_o if combo.all? { |combo_num| player_numbers.include?(combo_num) }
+      return x_or_o if combo.all? { |combo_num| player_squares.include?(combo_num) }
     end
-    @winner = :tie unless @current_board.values.any? { |num| num == :b } || @winner != nil
-    @winner
+    :tie unless current_board.any? { |square_value| square_value == :b }
   end
-
-  def update_current_board(square, x_or_o)
-    @current_board[square] = x_or_o
-    @square_index.map! {|num| num == square.to_s ? " " : num }
-  end
-
 end
 
 class Human
-  def select_square(square_index)
+  def select_square(current_board)
     loop do
-      # User selects square
-      selection = gets.chomp
-      # Check validity of choice
-      if square_index.include?(selection)
-        return selection.to_i
+      user_selection = gets.chomp.to_i
+      return user_selection if (1..9).include?(user_selection) && current_board[user_selection - 1] == :b 
+      if (1..9).include?(user_selection)
+        puts "\nThat square has been taken. Please select another square"
       else
-        if (1..9).include?(selection.to_i)
-          puts "\nThat square has been taken. Please select another square"
-        else
-          puts "\nThat is not a valid selection. Please select a number between 1 and 9."
-        end
+        puts "\nThat is not a valid selection. Please select a number between 1 and 9."
       end
     end
   end
 end
 
 class Computer
-  WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  include GameEvaluation
 
   attr_reader :difficulty
 
@@ -153,70 +178,72 @@ class Computer
 
   def select_square(current_board)
     sleep 1
-    return current_board.select { |num, sq| sq == :b }.keys.sample if @difficulty == "easy"
-    user_numbers = current_board.select { |num, sq| sq == :x }.keys
-    computer_numbers = current_board.select { |num, sq| sq == :o }.keys
-    # Check for critical offensive square
-    computer_spot = find_critical_square(computer_numbers, user_numbers)
-    # Check for critical defensive square
-    computer_spot ||= find_critical_square(user_numbers, computer_numbers)
-    # Determine best square if none critical
-    computer_spot ||= find_best_square(user_numbers, computer_numbers)
-    # Starting possibilities
-    computer_spot ||= 5 if current_board[5] == :b
-    computer_spot ||= current_board.select { |num, sq| [1, 3, 7, 9].include?(num) && sq == :b }.keys.sample\
-                      unless current_board.select { |num, sq| [1, 3, 7, 9].include?(num) && sq == :b }.keys.empty?
-    computer_spot ||= current_board.select { |num, sq| sq == :b }.keys.sample
+    empty_squares = moves_for(current_board, :b)
+    human_squares = moves_for(current_board, :x)
+    computer_squares = moves_for(current_board, :o)
+    return empty_squares.sample if difficulty == "easy"
+    ai_square_selection(empty_squares, human_squares, computer_squares)
   end
 
-  def find_critical_square(p1_numbers, p2_numbers)
+  def ai_square_selection(empty_squares, human_squares, computer_squares)
+    ai_selection = find_winning_square(computer_squares, human_squares)
+    ai_selection ||= find_winning_square(human_squares, computer_squares)
+    ai_selection ||= find_best_square(human_squares, computer_squares)
+    ai_selection ||= 4 if empty_squares.include?(4)
+    ai_selection ||= empty_squares.select { |num| [0, 2, 6, 8].include?(num) }.sample
+    ai_selection ||= empty_squares.sample
+  end
+
+  def find_winning_square(p1_squares, p2_squares)
     WINNING_COMBOS.each do |combo|
-      i = 0
-      strategic_square = nil
-      combo.each do |sq|
-        if p1_numbers.include?(sq)
-          i += 1
-        else
-          strategic_square = sq unless p2_numbers.include?(sq)
-        end
-      end
-      return strategic_square if strategic_square && i == 2
+      player_squares_in_combo = combo.select { |sq| p1_squares.include?(sq) }.length
+      return (combo - p1_squares)[0] if player_squares_in_combo == 2 && (combo - p2_squares) == combo
     end
     nil
   end
   
-  def find_best_square(user_numbers, computer_numbers)
-    users_winning_combos = WINNING_COMBOS.select do |combo|
-      combo.all? { |sq| !computer_numbers.include?(sq) } && combo.any? { |sq| user_numbers.include?(sq) }
+  def find_best_square(human_squares, computer_squares)
+    humans_winning_combos = determine_winning_combos(human_squares, computer_squares)
+    computers_winning_combos = determine_winning_combos(computer_squares, human_squares)
+    fork_opportunities = find_fork_opportunities(humans_winning_combos, human_squares)
+    best_square_based_on_forks(computer_squares, computers_winning_combos, fork_opportunities)
+  end
+  
+  def determine_winning_combos(p1_squares, p2_squares)
+    WINNING_COMBOS.select do |combo|
+      combo.all? { |combo_sq| !p2_squares.include?(combo_sq) } && combo.any? { |combo_sq| p1_squares.include?(combo_sq) }
     end
-    computers_winning_combos = WINNING_COMBOS.select do |combo|
-      combo.all? { |sq| !user_numbers.include?(sq) } && combo.any? { |sq| computer_numbers.include?(sq) }
-    end
-    fork_opportunities = find_fork_opportunities(users_winning_combos, user_numbers)
-    # If two fork opportunities avoid block, play offense / if one opportunity block
+  end
+  
+  def best_square_based_on_forks(computer_squares, computers_winning_combos, fork_opportunities)
     if fork_opportunities.length == 2
-      computers_winning_combos.flatten.select { |num| !computer_numbers.include?(num) && !fork_opportunities.include?(num) }.sample
+      computers_winning_combos.flatten.select { |combo_sq| !(computer_squares + fork_opportunities).include?(combo_sq) }.sample
     elsif fork_opportunities.length > 1
-      fork_opportunities.select { |num|  computers_winning_combos.flatten.include?(num) }.sample
+      fork_opportunities.select { |fork_sq|  computers_winning_combos.flatten.include?(fork_sq) }.sample
     else
       fork_opportunities[0]
     end
   end
 
-  def find_fork_opportunities(users_winning_combos, user_numbers)
+  def find_fork_opportunities(humans_winning_combos, human_squares)
     fork_opportunities = []
-    users_winning_combos.each do |combo|
-      combo.each do |num|
-        i = 0
-        users_winning_combos.each do |combo_reference|
-          i += 1 if combo_reference.include?(num) && !user_numbers.include?(num)
-        end
-        if i > 1
-          fork_opportunities << num unless fork_opportunities.include?(num)
-        end
+    humans_winning_combos.each do |combo|
+      combo.each do |sq|
+        fork_opportunities << sq if winning_combos_overlap_at_square?(humans_winning_combos, human_squares, sq, fork_opportunities)
       end
     end
     fork_opportunities
+  end
+
+  def winning_combos_overlap_at_square?(humans_winning_combos, human_squares, sq, fork_opportunities)
+    combos_with_sq = 0
+    humans_winning_combos.each do |combo_reference|
+      combos_with_sq += 1 if (combo_reference - human_squares).include?(sq)
+    end
+    if combos_with_sq > 1
+      return true unless fork_opportunities.include?(sq)
+    end
+    false
   end
 end
 
